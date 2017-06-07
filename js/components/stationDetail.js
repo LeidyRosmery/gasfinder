967776074a60573d65ea756a78bf3de73cb93fc9
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-07 01:56:57 -0500

    funcion volver

--- js/components/stationDetail.js
+++ js/components/stationDetail.js
@@ -2,9 +2,18 @@
 
 const StationDetail = (update) => {
   const container = $('<div class="stationDetail"></div>');
-  const title = $('<h1>'+state.selectedStation.name+'</h1>');
+  const nameStation = $('<h1>'+state.selectedStation.name+'</h1>');
+  const divisor=$('<hr>');
+  const directionStation = $('<h1>'+state.selectedStation.address+'</h1>');
+  container.append(nameStation);
+  container.append(divisor);
+  container.append(directionStation);
+  state.selectedStation.products.forEach((elemento,indice)=>{
+    const producto=$('<button>'+elemento+'</button>');
+      container.append(producto);
+  });
   const button = $('<button>Regresar</button>')
-  container.append(title);
+
   container.append(button);
 
   button.on('click',(e) => {
